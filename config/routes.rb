390b1bbeeb43e9f5b43eac06839df8c1983abf3e
Tom Ward <tom.ward@transportapi.com>
2015-02-04 12:56:04 +0000

    Fixed the routes to work with rails 4.

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ CodepointGeocodeApi::Application.routes.draw do
 
 
    # show a postcode
-   match 'postcodes/:postcode' => 'postcodes#show'
+   get 'postcodes/:postcode' => 'postcodes#show'
    # find nearest postcode(s)
-   match 'postcodes' => 'postcodes#near'
+   get 'postcodes' => 'postcodes#near'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  #   get 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  #   get 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
@@ -61,5 +61,5 @@ CodepointGeocodeApi::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  # get ':controller(/:action(/:id))(.:format)'
 end
