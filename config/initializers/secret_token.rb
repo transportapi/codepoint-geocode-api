d4ac59eea5513fa96a5aa71a72cd2aad382a8195
Tom Ward <tom.ward@transportapi.com>
2019-09-20 16:21:12 +0100

    Add secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 CodepointGeocodeApi::Application.config.secret_token = 'c8256a82760e8de618f56636d1fff33e2bd4009ed57576b8086e278baee64dc3fc7a4f334636de94f15b985cb1bfe472e8bd3a475dcb12916385ca03bbcc747e'
+
+CodepointGeocodeApi::Application.config.secret_key_base = '40237ebfc02df92342053312b7481c1af7282409600af05b8d09eca8594932454f52cdfdd3c38571c3093cbce7d06f47fd4f00e7e564630b7f8dc9178ed53752'
